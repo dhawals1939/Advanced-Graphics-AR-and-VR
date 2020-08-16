884b1eb854ebd2c82c818a3322dc32e6ded7fbb0
thesigmaguy <sdhawal2k8@gmail.com>
2020-08-16 13:35:16 +0530

    three-done

--- Assignment0/pentagonal_pyramid.py
+++ Assignment0/pentagonal_pyramid.py
@@ -1,40 +1,17 @@
-import moderngl_window
 import moderngl
+import moderngl_window
+from pathlib import Path
+import numpy as np
 import glm
 import math
-import numpy as np
-from pathlib import Path
-
 
 class pentagonal_pyramid(moderngl_window.WindowConfig):
-    gl_version = (4, 3)
-    window_size = (1920, 1080)
-    resource_dir = Path('.').absolute()
-    resizable = True
-    title = 'Pentagonal Pyramid'
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-        self.program = self.ctx.program(vertex_shader=open('./pentagonal_pyramid.vert.glsl').read(),
-                                        fragment_shader=open('./pentagonal_pyramid.frag.glsl').read())
-
-        godel_ratio = (1 + math.sqrt(5)) / 2
-
-import moderngl
-import moderngl_window
-from pathlib import Path
-import numpy as np
-import glm
-import math
-
-class camera(moderngl_window.WindowConfig):
     gl_version = (4, 3)
     resource_dir = Path('.').absolute()
     resizable = True
     window_size = (1920, 1080)
     aspect_ratio = 16 / 9
-    title = "Camera"
+    title = "pentagonal_pyramid"
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -42,11 +19,7 @@ class camera(moderngl_window.WindowConfig):
         self.program = self.ctx.program(vertex_shader=open("./pentagonal_pyramid.vert.glsl").read(),
                                         fragment_shader=open("./pentagonal_pyramid.frag.glsl").read())
 
-        # self.texture_bg = self.load_texture_2d('../imgs/wall.jpg')
-        # self.texture_fg = self.load_texture_2d('../imgs/awesomeface.png')
-
-        # self.program['out_tex_bg'] = 0
-        # self.program['out_tex_fg'] = 1
+        self.world = glm.mat4(1.0)
 
         self.box_vertices = np.array(
             [
@@ -64,19 +37,25 @@ class camera(moderngl_window.WindowConfig):
                 # pentagon sides
                 .5, 0, -.5,
                 0, .5, -.5,
-                0, 0, 0,
+                0, 0, 0.5,
 
-                0, 0, 0,
+                0, 0, 0.5,
                 0, .5, -.5,
                 -.5, 0, -.5,
 
                 -.5, 0, -.5,
                 -.5, -.5, -.5,
-                0, 0, 0,
+                0, 0, 0.5,
 
-                0, 0, 0,
+                0, 0, 0.5,
+                - .5, -.5, -.5,
                 .5, -.5, -.5,
-                - .5, -.5, -.5
+
+                .5, -.5, -.5,
+                .5, 0, -.5,
+                0, 0, 0.5
+
+
             ]
         )
 
@@ -108,7 +87,11 @@ class camera(moderngl_window.WindowConfig):
 
                 0.5, 0.5, 0.5,
                 .0, .0, 1.,
-                .5, .5, 0
+                .5, .5, 0,
+
+                0, 0, 1.,
+                1, 0, 0,
+                .5, .5, .5
             ]
         )
 
@@ -126,27 +109,25 @@ class camera(moderngl_window.WindowConfig):
         self.ctx.clear(.0, .0, .0)
         self.ctx.enable(moderngl.DEPTH_TEST)
 
-        model, view = glm.mat4(1.0), glm.mat4(1.0)
-
-        # self.texture_bg.use(0)
-        # self.texture_fg.use(1)
-
-        radius = 10.0
-        view = glm.lookAt(glm.vec3(math.sin(time) * radius, .0, math.cos(time) * radius),
-                          glm.vec3(.0, .0, .0), glm.vec3(.0, 1., .0))
-
-        projection = glm.perspective(glm.radians(45.0), self.aspect_ratio, 0.1, 100.)
-
-        self.program["projection"].write(projection)
-
-        self.program['view'].write(view)
-        model = glm.translate(model, glm.vec3(0.0, 0.0, 0.0))
-        angle = 20.
-        model = glm.rotate(model, glm.radians(angle), glm.vec3(1., .3, .5))
-
-        self.program['model'].write(model)
+        self.program['world'].write(self.world)
         self.vao.render()
-        # for i in range(len(self.cube_positions)):
+
+    def key_event(self, key, action, modifiers):
+        keys = self.wnd.keys
+        current_postion = glm.vec3(self.world[0][3], self.world[1][3], self.world[2][3])
+        if action == keys.ACTION_PRESS:
+            if key == keys.W:       #Z into the screen
+                self.world = glm.translate(self.world, glm.vec3(self.world[0][3], self.world[1][3], self.world[2][3] - .1))
+            if key == keys.S:
+                self.world = glm.translate(self.world, glm.vec3(self.world[0][3], self.world[1][3], self.world[2][3] + .1))
+            if key == keys.A:       # X right of screen
+                self.world = glm.translate(self.world, glm.vec3(self.world[0][3] - .1, self.world[1][3], self.world[2][3]))
+            if key == keys.D:
+                self.world = glm.translate(self.world, glm.vec3(self.world[0][3] + .1, self.world[1][3], self.world[2][3]))
+            if key == keys.R:
+                self.world = glm.translate(self.world, glm.vec3(self.world[0][3], self.world[1][3] + .1, self.world[2][3]))
+            if key == keys.F:
+                self.world = glm.translate(self.world, glm.vec3(self.world[0][3], self.world[1][3] - .1, self.world[2][3]))
 
 
     @classmethod
@@ -154,4 +135,4 @@ class camera(moderngl_window.WindowConfig):
         moderngl_window.run_window_config(cls)
 
 
-camera.run()
+pentagonal_pyramid.run()
--- Assignment0/pentagonal_pyramid.vert.glsl
+++ Assignment0/pentagonal_pyramid.vert.glsl
@@ -3,13 +3,13 @@
 in vec3 in_vert;
 in vec3 in_color;
 
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
+uniform mat4 world;
+//uniform mat4 view;
+//uniform mat4 projection;
 
 out vec3 frag_in_color;
 void main()
 {
-	gl_Position = projection * view * model * vec4(in_vert, 1.0);
+	gl_Position = world * vec4(in_vert, 1.0);
 	frag_in_color = in_color;
 }
